33094ad1e0f781a730588b433be42862df16b12f
Moisés Jurado <moises003@gmail.com>
2023-09-08 18:37:13 -0500

    stable

--- cronner.py
+++ cronner.py
@@ -1,61 +1,27 @@
-import tkinter as tk
-from tkinter import ttk
 import time
 from plyer import notification
 
-class CronometroApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Cronómetro")
-        
-        self.tiempo_total = tk.StringVar()
-        self.cantidad_cortes = tk.StringVar()
-        self.tipo_configuracion = tk.StringVar()
-        self.tipo_configuracion.set("Cantidad de Cortes")
-        
-        self.inicializar_interfaz()
-        
-    def inicializar_interfaz(self):
-        # Etiqueta y entrada para configurar el tiempo total
-        ttk.Label(self.root, text="Tiempo Total (segundos):").grid(row=0, column=0)
-        ttk.Entry(self.root, textvariable=self.tiempo_total).grid(row=0, column=1)
-        
-        # Etiqueta y entrada para configurar la cantidad de cortes
-        ttk.Label(self.root, text="Cantidad de Cortes:").grid(row=1, column=0)
-        ttk.Entry(self.root, textvariable=self.cantidad_cortes).grid(row=1, column=1)
-        
-        # RadioButtons para seleccionar el tipo de configuración
-        ttk.Radiobutton(self.root, text="Cantidad de Cortes", variable=self.tipo_configuracion, value="Cantidad de Cortes").grid(row=2, column=0)
-        ttk.Radiobutton(self.root, text="Segundos entre Cortes", variable=self.tipo_configuracion, value="Segundos entre Cortes").grid(row=2, column=1)
-        
-        # Botón para iniciar el cronómetro
-        ttk.Button(self.root, text="Iniciar", command=self.iniciar_cronometro).grid(row=3, columnspan=2)
-        
-    def iniciar_cronometro(self):
-        tiempo_total = int(self.tiempo_total.get())
-        cantidad_cortes = int(self.cantidad_cortes.get())
-        tipo_configuracion = self.tipo_configuracion.get()
-        
-        if tipo_configuracion == "Cantidad de Cortes":
-            tiempo_entre_cortes = tiempo_total / cantidad_cortes
-        else:
-            tiempo_entre_cortes = int(self.tiempo_entre_cortes.get())
-        
-        for i in range(cantidad_cortes):
-            tiempo_restante = tiempo_total - i * tiempo_entre_cortes
-            self.mostrar_notificacion(f"Corte {i+1}", f"Tiempo Restante: {tiempo_restante} segundos")
-            time.sleep(tiempo_entre_cortes)
-        
-        self.mostrar_notificacion("Cronómetro Finalizado", "Tiempo Total Agotado")
-        
-    def mostrar_notificacion(self, titulo, mensaje):
-        notification.notify(
-            title=titulo,
-            message=mensaje,
-            app_name="Cronómetro App"
-        )
+# Configuración del cronómetro
+total_time = 15 * 60  # 15 minutos en segundos
+total_cuts = 50
+cut_interval = total_time / total_cuts  # Intervalo de tiempo entre cortes
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = CronometroApp(root)
-    root.mainloop()
+# Iniciar el cronómetro
+for cut_number in range(1, total_cuts + 1):
+    remaining_time = total_time - (cut_number - 1) * cut_interval
+    minutes, seconds = divmod(int(remaining_time), 60)
+
+    # Mostrar notificación
+    notification_title = "Cronómetro"
+    notification_message = f"Corte #{cut_number}: {minutes} minutos {seconds} segundos"
+    notification.notify(
+        title=notification_title,
+        message=notification_message,
+        app_name="CronometroApp",
+    )
+
+    if cut_number < total_cuts:
+        # Esperar hasta el próximo corte
+        time.sleep(cut_interval)
+
+print("Cronómetro finalizado")
