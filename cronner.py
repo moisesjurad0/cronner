9e963d4997081e443db6fffced5ab84e4046e034
Moisés Jurado <moises003@gmail.com>
2023-09-08 18:19:20 -0500

    v2

--- cronner.py
+++ cronner.py
@@ -1,7 +1,7 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import ttk
 import time
-import threading
+from plyer import notification
 
 class CronometroApp:
     def __init__(self, root):
@@ -9,65 +9,51 @@ class CronometroApp:
         self.root.title("Cronómetro")
         
         self.tiempo_total = tk.StringVar()
-        self.tiempo_total.set("00:00")
+        self.cantidad_cortes = tk.StringVar()
+        self.tipo_configuracion = tk.StringVar()
+        self.tipo_configuracion.set("Cantidad de Cortes")
         
-        self.label_tiempo = tk.Label(root, textvariable=self.tiempo_total, font=("Helvetica", 48))
-        self.label_tiempo.pack()
+        self.inicializar_interfaz()
         
-        self.configurar_btn = tk.Button(root, text="Configurar Tiempo", command=self.configurar_tiempo)
-        self.configurar_btn.pack()
+    def inicializar_interfaz(self):
+        # Etiqueta y entrada para configurar el tiempo total
+        ttk.Label(self.root, text="Tiempo Total (segundos):").grid(row=0, column=0)
+        ttk.Entry(self.root, textvariable=self.tiempo_total).grid(row=0, column=1)
         
-        self.iniciar_btn = tk.Button(root, text="Iniciar", command=self.iniciar_cronometro)
-        self.iniciar_btn.pack()
+        # Etiqueta y entrada para configurar la cantidad de cortes
+        ttk.Label(self.root, text="Cantidad de Cortes:").grid(row=1, column=0)
+        ttk.Entry(self.root, textvariable=self.cantidad_cortes).grid(row=1, column=1)
         
-        self.detener_btn = tk.Button(root, text="Detener", command=self.detener_cronometro, state=tk.DISABLED)
-        self.detener_btn.pack()
+        # RadioButtons para seleccionar el tipo de configuración
+        ttk.Radiobutton(self.root, text="Cantidad de Cortes", variable=self.tipo_configuracion, value="Cantidad de Cortes").grid(row=2, column=0)
+        ttk.Radiobutton(self.root, text="Segundos entre Cortes", variable=self.tipo_configuracion, value="Segundos entre Cortes").grid(row=2, column=1)
         
-        self.tiempo_transcurrido = 0
-        self.tiempo_configurado = 0
-        self.alertas_configuradas = []
-        self.cronometro_activo = False
-
-    def configurar_tiempo(self):
-        if not self.cronometro_activo:
-            tiempo_configurado = simpledialog.askstring("Configurar Tiempo", "Ingresa el tiempo total (en segundos):")
-            if tiempo_configurado is not None:
-                self.tiempo_configurado = int(tiempo_configurado)
-                self.tiempo_total.set(self.formato_tiempo(self.tiempo_configurado))
-
+        # Botón para iniciar el cronómetro
+        ttk.Button(self.root, text="Iniciar", command=self.iniciar_cronometro).grid(row=3, columnspan=2)
+        
     def iniciar_cronometro(self):
-        if self.tiempo_configurado > 0 and not self.cronometro_activo:
-            self.cronometro_activo = True
-            self.detener_btn.config(state=tk.NORMAL)
-            self.iniciar_btn.config(state=tk.DISABLED)
-            self.alertas_configuradas = [self.tiempo_configurado // 4, self.tiempo_configurado // 2, self.tiempo_configurado * 3 // 4]
-            self.actualizar_cronometro()
-
-    def detener_cronometro(self):
-        self.cronometro_activo = False
-        self.detener_btn.config(state=tk.DISABLED)
-        self.iniciar_btn.config(state=tk.NORMAL)
-
-    def actualizar_cronometro(self):
-        if self.cronometro_activo:
-            self.tiempo_transcurrido += 1
-            tiempo_restante = self.tiempo_configurado - self.tiempo_transcurrido
-            self.tiempo_total.set(self.formato_tiempo(tiempo_restante))
-            
-            if self.tiempo_transcurrido in self.alertas_configuradas:
-                messagebox.showinfo("Alerta", f"Ha transcurrido {self.tiempo_transcurrido} segundos.")
-            
-            if self.tiempo_transcurrido >= self.tiempo_configurado:
-                self.cronometro_activo = False
-                self.detener_btn.config(state=tk.DISABLED)
-                self.iniciar_btn.config(state=tk.NORMAL)
-            else:
-                self.root.after(1000, self.actualizar_cronometro)
-
-    def formato_tiempo(self, segundos):
-        minutos = segundos // 60
-        segundos = segundos % 60
-        return f"{minutos:02d}:{segundos:02d}"
+        tiempo_total = int(self.tiempo_total.get())
+        cantidad_cortes = int(self.cantidad_cortes.get())
+        tipo_configuracion = self.tipo_configuracion.get()
+        
+        if tipo_configuracion == "Cantidad de Cortes":
+            tiempo_entre_cortes = tiempo_total / cantidad_cortes
+        else:
+            tiempo_entre_cortes = int(self.tiempo_entre_cortes.get())
+        
+        for i in range(cantidad_cortes):
+            tiempo_restante = tiempo_total - i * tiempo_entre_cortes
+            self.mostrar_notificacion(f"Corte {i+1}", f"Tiempo Restante: {tiempo_restante} segundos")
+            time.sleep(tiempo_entre_cortes)
+        
+        self.mostrar_notificacion("Cronómetro Finalizado", "Tiempo Total Agotado")
+        
+    def mostrar_notificacion(self, titulo, mensaje):
+        notification.notify(
+            title=titulo,
+            message=mensaje,
+            app_name="Cronómetro App"
+        )
 
 if __name__ == "__main__":
     root = tk.Tk()
